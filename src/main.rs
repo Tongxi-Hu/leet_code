84415240d7ac0bd858e77e92c791a8cee4c59231
tongxi_hu <tongxi_hu@jiliguala.com>
2024-08-23 13:14:46 +0800

    101 102

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod common;
 mod problem_100;
+mod problem_200;
 
 fn main() {
     println!("Hello, world!");
--- src/problem_200.rs
+++ src/problem_200.rs
@@ -1,1 +1,51 @@
+use std::{cell::RefCell, rc::Rc};
 
+use crate::common::TreeNode;
+
+///101
+/// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+    fn check(left: &Option<Rc<RefCell<TreeNode>>>, right: &Option<Rc<RefCell<TreeNode>>>) -> bool {
+        match (left, right) {
+            (None, None) => true,
+            (Some(_), None) => false,
+            (None, Some(_)) => false,
+            (Some(left_node), Some(right_node)) => {
+                return left_node.borrow().val == right_node.borrow().val
+                    && check(&left_node.borrow().left, &right_node.borrow().right)
+                    && check(&left_node.borrow().right, &right_node.borrow().left);
+            }
+        }
+    }
+    match root {
+        None => return true,
+        Some(root_node) => return check(&root_node.borrow().left, &root_node.borrow().right),
+    }
+}
+
+/// p102
+/// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+pub fn level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+    let mut val: Vec<Vec<i32>> = vec![];
+    let mut current_level: Vec<Rc<RefCell<TreeNode>>> = vec![];
+    if let Some(node) = root {
+        current_level.push(node);
+    }
+    while !current_level.is_empty() {
+        let mut next = vec![];
+        let mut values = vec![];
+        for cur in current_level {
+            let mut node = cur.borrow_mut();
+            values.push(node.val);
+            if let Some(left) = node.left.take() {
+                next.push(left);
+            }
+            if let Some(right) = node.right.take() {
+                next.push(right);
+            }
+        }
+        val.push(values);
+        current_level = next;
+    }
+    return val;
+}
