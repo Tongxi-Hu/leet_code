89cc698b9497a9132148ec60a45fe81ca76bab1e
tongxi_hu <tongxi_hu@jiliguala.com>
2024-08-27 13:36:56 +0800

    103 104

--- src/problem_200.rs
+++ src/problem_200.rs
@@ -51,3 +51,51 @@ pub fn level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
     }
     return val;
 }
+
+///p103
+///
+///Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+pub fn zigzag_level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+    let mut ans: Vec<Vec<i32>> = vec![];
+    let mut current_level: Vec<Rc<RefCell<TreeNode>>> = vec![];
+    let mut from_left = true;
+    if let Some(val) = root {
+        current_level.push(val);
+    }
+    while !current_level.is_empty() {
+        let mut next = vec![];
+        let mut values = vec![];
+        for item in current_level {
+            let mut node = item.borrow_mut();
+            values.push(node.val);
+            if let Some(left) = node.left.take() {
+                next.push(left)
+            }
+            if let Some(right) = node.right.take() {
+                next.push(right)
+            }
+        }
+        current_level = next;
+        if from_left {
+            ans.push(values);
+        } else {
+            values.reverse();
+            ans.push(values);
+        }
+        from_left = !from_left;
+    }
+    return ans;
+}
+
+///p104
+///
+///A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    let mut depth = 0;
+    if let Some(val) = root {
+        depth = 1;
+        let mut node = val.borrow_mut();
+        depth = depth + max_depth(node.left.take()).max(max_depth(node.right.take()));
+    }
+    return depth;
+}
