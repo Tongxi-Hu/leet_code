61cf9fb279fa84ce8d1b3cf00f17889a3e3fad03
tongxi_hu <tongxi_hu@jiliguala.com>
2024-09-11 12:03:50 +0800

    149 150

--- src/problem_200.rs
+++ src/problem_200.rs
@@ -1010,3 +1010,87 @@ pub fn sort_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         return head;
     }
 }
+
+///p149
+///
+/// Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
+fn gcd(mut a: i32, mut b: i32) -> i32 {
+    while b != 0 {
+        let c = a % b;
+        a = b;
+        b = c;
+    }
+    a
+}
+pub fn max_points(points: Vec<Vec<i32>>) -> i32 {
+    use std::collections::HashMap;
+    let mut best = 0;
+    for (i, p1) in points.iter().enumerate() {
+        let (x1, y1) = (p1[0], p1[1]);
+
+        // 记录一条直线出现的次数
+        let mut cnts = HashMap::<(i32, i32, i32), i32>::new();
+        // 从下标i + 1开始遍历
+        for p2 in points.iter().skip(i + 1) {
+            let (x2, y2) = (p2[0], p2[1]);
+            let v3 = [y2 - y1, x1 - x2, x2 * y1 - x1 * y2];
+            // 记录第一个非零元素的下标
+            let mut nzi = -1;
+            for k in 0..3 {
+                if v3[k] != 0 {
+                    nzi = k as i32;
+                    break;
+                }
+            }
+            let mut g = 1;
+            if nzi != -1 {
+                // 约分
+                g = v3.iter().fold(v3[nzi as usize], |g, &x| {
+                    // 不需要考虑0
+                    if x == 0 {
+                        g
+                    } else {
+                        gcd(g, x)
+                    }
+                });
+                g = g.abs();
+                if v3[nzi as usize] < 0 {
+                    g = -g;
+                }
+            }
+            // 进行约分并转为元组类型
+            let v3 = (v3[0] / g, v3[1] / g, v3[2] / g);
+            *cnts.entry(v3).or_insert(0) += 1;
+            best = best.max(cnts[&v3]);
+        }
+    }
+    best + 1
+}
+
+///p150
+///
+/// Evaluate the expression. Return an integer that represents the value of the expression.
+///
+/// Input: tokens = ["4","13","5","/","+"]
+///
+/// Output: 6
+///
+/// Explanation: (4 + (13 / 5)) = 6
+pub fn eval_rpn(tokens: Vec<String>) -> i32 {
+    let mut number_stack: Vec<i32> = vec![];
+    for token in tokens {
+        if let Ok(number) = token.parse::<i32>() {
+            number_stack.push(number);
+        } else {
+            let right = number_stack.pop().unwrap();
+            let left = number_stack.pop().unwrap();
+            number_stack.push(match token.as_str() {
+                "+" => left + right,
+                "-" => left - right,
+                "*" => left * right,
+                _ => left / right,
+            })
+        }
+    }
+    return number_stack[0];
+}
