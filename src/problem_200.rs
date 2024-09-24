98c04dea39dc8e7632736486a7b21547a7abc831
tongxi hu <ryan.proning@gmail.com>
2024-09-24 20:19:24 +0800

    187

--- src/problem_200.rs
+++ src/problem_200.rs
@@ -1467,3 +1467,21 @@ pub fn largest_number(mut nums: Vec<i32>) -> String {
     }
     nums.iter().map(|i| i.to_string()).collect()
 }
+
+///p187
+pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
+    if s.len() < 10 {
+        return vec![];
+    }
+    let mut cnt = std::collections::HashMap::new();
+    let mut ans = Vec::new();
+    for i in 0..s.len() - 9 {
+        let t = &s[i..i + 10];
+        let count = cnt.entry(t).or_insert(0);
+        *count += 1;
+        if *count == 2 {
+            ans.push(t.to_string());
+        }
+    }
+    ans
+}
