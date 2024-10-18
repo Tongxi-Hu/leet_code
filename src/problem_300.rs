914cdd75286fe099f077730fea37857b2dad22d1
tongxi_hu <tongxi_hu@jiliguala.com>
2024-10-18 18:44:58 +0800

    201

--- src/problem_300.rs
+++ src/problem_300.rs
@@ -1,3 +1,30 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, collections::HashSet, rc::Rc};
 
 use crate::common::{ListNode, TreeNode};
+
+///p202
+pub fn is_happy(n: i32) -> bool {
+    let mut n = n;
+    let mut history: std::collections::HashSet<i32> = std::collections::HashSet::new();
+    while n != 1 {
+        n = n
+            .to_string()
+            .chars()
+            .map(|char| char.to_digit(10).unwrap())
+            .collect::<Vec<u32>>()
+            .into_iter()
+            .fold(0, |acc, cur| return acc + (cur * cur) as i32);
+        if n == 1 {
+            return true;
+        };
+        match history.get(&n) {
+            Some(_) => {
+                return false;
+            }
+            None => {
+                history.insert(n);
+            }
+        }
+    }
+    return true;
+}
