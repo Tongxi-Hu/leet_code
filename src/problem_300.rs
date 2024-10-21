951c825ced05245c763e534e04cd058395099a84
tongxi_hu <tongxi_hu@jiliguala.com>
2024-10-21 12:09:37 +0800

    207

--- src/problem_300.rs
+++ src/problem_300.rs
@@ -108,3 +108,35 @@ pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
     }
     return pre;
 }
+
+/// p207
+pub fn can_finish(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> bool {
+    let mut income = vec![0; num_courses as usize];
+    let mut map = std::collections::HashMap::new();
+    prerequisites.iter().for_each(|x| {
+        let (a, b) = (x[0] as usize, x[1] as usize);
+        income[a] += 1;
+        map.entry(b).or_insert(vec![]).push(a);
+    });
+    let mut courses = std::collections::VecDeque::new();
+    for (i, &n) in income.iter().enumerate() {
+        if n == 0 {
+            courses.push_back(i);
+        }
+    }
+
+    let mut ans = 0;
+    while !courses.is_empty() {
+        ans += 1;
+        let course = courses.pop_front().unwrap();
+        if let Some(next) = map.get(&course) {
+            for &n in next {
+                income[n] -= 1;
+                if income[n] == 0 {
+                    courses.push_back(n);
+                }
+            }
+        }
+    }
+    ans == num_courses
+}
