7e2a847bec6e4dfa4cacf5384b93b7ea62009718
tongxi_hu <tongxi_hu@jiliguala.com>
2024-11-08 12:24:21 +0800

    299 300

--- src/problem_300.rs
+++ src/problem_300.rs
@@ -1528,3 +1528,39 @@ impl Codec {
         )
     }
 }
+
+///p299
+pub fn get_hint(secret: String, guess: String) -> String {
+    let mut a = 0;
+    let mut cnt_s = [0; 10];
+    let mut cnt_g = [0; 10];
+    for (s, g) in secret.bytes().zip(guess.bytes()) {
+        if s == g {
+            a += 1;
+        } else {
+            cnt_s[(s - b'0') as usize] += 1;
+            cnt_g[(g - b'0') as usize] += 1;
+        }
+    }
+    let b = cnt_s
+        .into_iter()
+        .zip(cnt_g)
+        .map(|(s, g)| s.min(g))
+        .sum::<i32>();
+    format!("{}A{}B", a, b)
+}
+
+///p300
+pub fn length_of_lis(nums: Vec<i32>) -> i32 {
+    let mut dp: Vec<usize> = vec![1; nums.len()];
+    for i in 1..nums.len() {
+        let mut temp_length = 0;
+        for j in 0..i {
+            if nums[i] > nums[j] {
+                temp_length = temp_length.max(dp[j]);
+            };
+        }
+        dp[i] = temp_length + 1;
+    }
+    return dp.into_iter().max().unwrap_or(1) as i32;
+}
