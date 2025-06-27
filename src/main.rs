f25eb3d0ebfcd10678dc4cbc5cad5225ab66ed95
tongxi_hu <tongxi_hu@jiliguala.com>
2025-06-27 13:32:51 +0800

    lint

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use crate::problem_500::license_key_formatting;
-
 mod common;
 mod problem_100;
 mod problem_200;
@@ -7,9 +5,4 @@ mod problem_300;
 mod problem_400;
 mod problem_500;
 
-fn main() {
-    println!(
-        "{:?}",
-        license_key_formatting(String::from("2-4A0r7-4k"), 3)
-    );
-}
+fn main() {}
