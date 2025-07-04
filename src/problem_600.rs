674d3e0ff2ca06e6a2c9c0e84efb53ba2173d326
tongxi_hu <tongxi_hu@jiliguala.com>
2025-07-04 13:44:48 +0800

    p506 507 508 509

--- src/problem_600.rs
+++ src/problem_600.rs
@@ -10,7 +10,7 @@ pub fn find_mode(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
     fn dfs(root: Rc<RefCell<TreeNode>>, frequncy: &mut HashMap<i32, i32>) {
         let count = frequncy.entry(root.borrow().val).or_insert(0);
         *count = *count + 1;
-        if let Some(left) = root.clone().borrow().left.as_ref() {
+        if let Some(left) = root.borrow().left.as_ref() {
             dfs(left.clone(), frequncy);
         }
         if let Some(right) = root.borrow().right.as_ref() {
@@ -108,4 +108,86 @@ pub fn convert_to_base7(num: i32) -> String {
     }
     symbols.reverse();
     symbols.iter().collect::<String>()
+}
+
+/// p506
+pub fn find_relative_ranks(score: Vec<i32>) -> Vec<String> {
+    let mut with_postion = score.into_iter().enumerate().collect::<Vec<(usize, i32)>>();
+    with_postion.sort_by(|a, b| b.1.cmp(&a.1));
+    let mut ranking: Vec<String> = vec!["".to_string(); with_postion.len()];
+    with_postion
+        .iter()
+        .enumerate()
+        .for_each(|(index, score)| match index {
+            0 => ranking[score.0] = "Gold Medal".to_string(),
+            1 => ranking[score.0] = "Silver Medal".to_string(),
+            2 => ranking[score.0] = "Bronze Medal".to_string(),
+            r => ranking[score.0] = (r + 1).to_string(),
+        });
+
+    ranking
+}
+
+/// p507
+pub fn check_perfect_number(num: i32) -> bool {
+    let mut divisior: Vec<i32> = vec![];
+    for i in 1..=num / 2 {
+        if num % i == 0 {
+            divisior.push(i)
+        }
+    }
+    divisior.iter().sum::<i32>() == num
+}
+
+/// p508
+pub fn find_frequent_tree_sum(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut sum_frequency: HashMap<i32, usize> = HashMap::new();
+    fn dfs(root: &Option<Rc<RefCell<TreeNode>>>, sum_frequency: &mut HashMap<i32, usize>) -> i32 {
+        if let Some(node) = root.as_ref() {
+            let mut sum = node.borrow().val;
+            sum = sum + dfs(&node.borrow().left, sum_frequency);
+            sum = sum + dfs(&node.borrow().right, sum_frequency);
+            let count = sum_frequency.entry(sum).or_insert(0);
+            *count = *count + 1;
+            return sum;
+        } else {
+            return 0;
+        }
+    }
+
+    dfs(&root, &mut sum_frequency);
+
+    let mut accurance: usize = 0;
+    let mut vals: Vec<i32> = vec![];
+    sum_frequency.iter().for_each(|(v, c)| {
+        if *c > accurance {
+            accurance = *c;
+            vals.clear();
+            vals.push(*v)
+        } else if *c == accurance {
+            vals.push(*v)
+        }
+    });
+    vals
+}
+
+/// p509
+pub fn fib(n: i32) -> i32 {
+    match n {
+        0 => return 0,
+        1 => return 1,
+        v => {
+            let mut sum_pre = 0;
+            let mut sum = 1;
+            let mut temp = 0;
+            for i in 0..=v {
+                if i >= 2 {
+                    temp = sum;
+                    sum = sum + sum_pre;
+                    sum_pre = temp;
+                }
+            }
+            return sum;
+        }
+    }
 }
