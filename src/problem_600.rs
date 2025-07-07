7319c5abd1fda6d604e6755971e7aae34c08f576
tongxi_hu <tongxi_hu@jiliguala.com>
2025-07-07 13:02:28 +0800

    p513 514 515

--- src/problem_600.rs
+++ src/problem_600.rs
@@ -1,4 +1,4 @@
-use std::cell::RefCell;
+use std::cell::{Ref, RefCell};
 use std::collections::{BTreeMap, BinaryHeap, HashMap};
 use std::rc::Rc;
 
@@ -163,9 +163,9 @@ pub fn find_frequent_tree_sum(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
         if *c > accurance {
             accurance = *c;
             vals.clear();
-            vals.push(*v)
+            vals.push(*v);
         } else if *c == accurance {
-            vals.push(*v)
+            vals.push(*v);
         }
     });
     vals
@@ -190,4 +190,84 @@ pub fn fib(n: i32) -> i32 {
             return sum;
         }
     }
+}
+
+/// p513
+pub fn find_bottom_left_value(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    let mut ans: i32 = 0;
+    if let Some(val) = root.as_ref() {
+        let mut queue: Vec<Rc<RefCell<TreeNode>>> = vec![val.clone()];
+        while queue.len() != 0 {
+            let first = queue.remove(0);
+            if let Some(right) = first.borrow().right.as_ref() {
+                queue.push(right.clone());
+            }
+            if let Some(left) = first.borrow().left.as_ref() {
+                queue.push(left.clone());
+            }
+            ans = first.borrow().val;
+        }
+    }
+    ans
+}
+
+/// p514
+pub fn find_rotate_steps(ring: String, key: String) -> i32 {
+    let (n, m) = (ring.len(), key.len());
+    let (ring, key) = (
+        ring.chars().collect::<Vec<_>>(),
+        key.chars().collect::<Vec<_>>(),
+    );
+    // pos 用于记录 ring 中每个字符出现的所有位置
+    let mut pos = vec![vec![]; 26];
+    for (i, &c) in ring.iter().enumerate() {
+        pos[(c as u8 - b'a') as usize].push(i);
+    }
+    // dp[i][j] 表示拼写 key 的前 i 个字符，ring 的指针在 j 位置时的最小操作次数
+    let mut dp = vec![vec![std::i32::MAX; n]; m];
+
+    // 初始化 dp[0][...]，处理 key 的第一个字符
+    for &i in pos[(key[0] as u8 - b'a') as usize].iter() {
+        dp[0][i] = i.min(n - i) as i32 + 1; // +1 是按下按钮的操作
+    }
+
+    for i in 1..m {
+        // 当前字符在 ring 中的位置
+        for &j in pos[(key[i] as u8 - b'a') as usize].iter() {
+            // 上一个字符在 ring 中的位置
+            for &k in pos[(key[i - 1] as u8 - b'a') as usize].iter() {
+                dp[i][j] = std::cmp::min(
+                    dp[i][j],
+                    dp[i - 1][k]
+                            + std::cmp::min((j + n - k) % n, (k + n - j) % n) as i32 // 旋转 ring 的最小距离
+                            + 1, // +1 是按下按钮的操作
+                );
+            }
+        }
+    }
+    // 返回拼写完整个 key 时，ring 的指针在任意位置的最小操作次数
+    *dp[m - 1].iter().min().unwrap()
+}
+
+/// p515
+pub fn largest_values(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut ans: Vec<i32> = vec![];
+    if let Some(val) = root.as_ref() {
+        let mut queue: Vec<Rc<RefCell<TreeNode>>> = vec![val.clone()];
+        while queue.len() != 0 {
+            let mut max = std::i32::MIN;
+            for i in 0..queue.len() {
+                let first = queue.remove(0);
+                max = max.max(first.borrow().val);
+                if let Some(left) = first.borrow().left.as_ref() {
+                    queue.push(left.clone());
+                }
+                if let Some(right) = first.borrow().right.as_ref() {
+                    queue.push(right.clone());
+                }
+            }
+            ans.push(max);
+        }
+    }
+    ans
 }
