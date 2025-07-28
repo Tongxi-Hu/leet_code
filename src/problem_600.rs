2f581b7276ffbda38329952910e82d32fa9a04ae
tongxi_hu <tongxi_hu@jiliguala.com>
2025-07-28 12:19:35 +0800

    p576

--- src/problem_600.rs
+++ src/problem_600.rs
@@ -1072,3 +1072,34 @@ pub fn distribute_candies(candy_type: Vec<i32>) -> i32 {
     let set = candy_type.iter().collect::<HashSet<_>>();
     (candy_type.len() / 2).min(set.len()) as i32
 }
+
+/// p576
+pub fn find_paths(m: i32, n: i32, max_move: i32, start_row: i32, start_column: i32) -> i32 {
+    let mut dp = vec![vec![vec![0; n as usize]; m as usize]; max_move as usize + 1];
+    dp[0][start_row as usize][start_column as usize] = 1;
+    let mut out_counts = 0;
+    let mod_num = 1_000_000_007;
+
+    for i in 0..max_move {
+        for j in 0..m {
+            for k in 0..n {
+                if dp[i as usize][j as usize][k as usize] > 0 {
+                    for &(dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
+                        let new_x = j + dx;
+                        let new_y = k + dy;
+                        if new_x >= 0 && new_x < m && new_y >= 0 && new_y < n {
+                            dp[i as usize + 1][new_x as usize][new_y as usize] = (dp
+                                [i as usize + 1][new_x as usize][new_y as usize]
+                                + dp[i as usize][j as usize][k as usize])
+                                % mod_num;
+                        } else {
+                            out_counts =
+                                (out_counts + dp[i as usize][j as usize][k as usize]) % mod_num;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    out_counts
+}
