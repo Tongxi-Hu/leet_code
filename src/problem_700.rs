0da2bc4a653565baa479b82c8cf8cbbe240d7d9a
tongxi_hu <tongxi_hu@jiliguala.com>
2025-08-05 12:31:01 +0800

    p 606 609

--- src/problem_700.rs
+++ src/problem_700.rs
@@ -1,3 +1,7 @@
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
+
+use crate::common::TreeNode;
+
 /// p605
 pub fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
     let m = flowerbed.len();
@@ -15,4 +19,53 @@ pub fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
         }
     }
     n <= 0
+}
+
+/// p606
+pub fn tree2str(root: Option<Rc<RefCell<TreeNode>>>) -> String {
+    fn dfs(root: &Option<Rc<RefCell<TreeNode>>>) -> Option<String> {
+        if let Some(node) = root.as_ref() {
+            let mut res = String::new();
+            let v = node.borrow().val;
+            res = res + &v.to_string();
+            let left_res = dfs(&node.borrow().left);
+            let right_res = dfs(&node.borrow().right);
+            if let Some(left_val) = left_res.as_ref() {
+                res = res + "(";
+                res = res + &left_val.to_string();
+                res = res + ")";
+            }
+            if let Some(right_val) = right_res {
+                if left_res == None {
+                    res = res + "()";
+                }
+                res = res + "(";
+                res = res + &right_val.to_string();
+                res = res + ")";
+            }
+            return Some(res);
+        } else {
+            None
+        }
+    }
+    dfs(&root).unwrap_or("".to_string())
+}
+
+/// p609
+pub fn find_duplicate(paths: Vec<String>) -> Vec<Vec<String>> {
+    let mut map: HashMap<String, Vec<String>> = HashMap::new();
+
+    for path in paths.into_iter() {
+        let vals = path.split(' ').collect::<Vec<_>>();
+        for &s in vals.iter().skip(1) {
+            let idx = s.find('(').unwrap();
+            let cur_path = vals[0].to_string() + "/" + &s[..idx];
+            let cur_content = s[idx..].to_string();
+            map.entry(cur_content).or_insert(Vec::new()).push(cur_path);
+        }
+    }
+
+    map.into_values()
+        .filter(|vals| vals.len() > 1)
+        .collect::<Vec<_>>()
 }
