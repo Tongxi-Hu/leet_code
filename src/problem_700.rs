a0dcf6ab6c5e770ff5799bc6d75728ac694e7bde
tongxi_hu <tongxi_hu@jiliguala.com>
2025-08-06 13:06:34 +0800

    p 611 617

--- src/problem_700.rs
+++ src/problem_700.rs
@@ -69,3 +69,48 @@ pub fn find_duplicate(paths: Vec<String>) -> Vec<Vec<String>> {
         .filter(|vals| vals.len() > 1)
         .collect::<Vec<_>>()
 }
+
+/// p611
+pub fn triangle_number(nums: Vec<i32>) -> i32 {
+    let mut nums = nums;
+    nums.sort();
+    let length = nums.len();
+    if length < 3 {
+        return 0;
+    }
+    let mut count = 0;
+    for i in 0..length - 2 {
+        for j in i + 1..length - 1 {
+            for k in j + 1..length {
+                if nums[i] + nums[j] > nums[k] {
+                    count = count + 1;
+                }
+            }
+        }
+    }
+    count
+}
+
+/// p617
+pub fn merge_trees(
+    root1: Option<Rc<RefCell<TreeNode>>>,
+    root2: Option<Rc<RefCell<TreeNode>>>,
+) -> Option<Rc<RefCell<TreeNode>>> {
+    fn dfs(
+        root1: &Option<Rc<RefCell<TreeNode>>>,
+        root2: &Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        match (root1.as_ref(), root2.as_ref()) {
+            (None, None) => None,
+            (Some(node1), Some(node2)) => {
+                let mut node = TreeNode::new(node1.borrow().val + node2.borrow().val);
+                node.left = dfs(&node1.borrow().left, &node2.borrow().left);
+                node.right = dfs(&node1.borrow().right, &node2.borrow().right);
+                Some(Rc::new(RefCell::new(node)))
+            }
+            (Some(node1), None) => Some(node1.clone()),
+            (None, Some(node2)) => Some(node2.clone()),
+        }
+    }
+    dfs(&root1, &root2)
+}
