1d081b8d5c88831eee5dca197562afa83b2dc4b0
tongxi hu <ryan.proning@gmail.com>
2025-10-08 16:34:15 +0800

    746 747 748 749

--- src/problem_800.rs
+++ src/problem_800.rs
@@ -1,6 +1,6 @@
 use std::{
     cell::RefCell,
-    cmp::Reverse,
+    cmp::{Ordering, Reverse},
     collections::{BTreeMap, BinaryHeap, HashMap, HashSet},
     rc::Rc,
 };
@@ -1331,3 +1331,187 @@ impl WordFilter {
         curr.index
     }
 }
+
+/// p746
+pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
+    let length = cost.len();
+    let mut dp = vec![0; length];
+    for i in 2..length {
+        dp[i] = (dp[i - 2] + cost[i - 2]).min(dp[i - 1] + cost[i - 1]);
+    }
+    (dp[length - 1] + cost[length - 1]).min(dp[length - 2] + cost[length - 2])
+}
+
+/// p747
+pub fn dominant_index(nums: Vec<i32>) -> i32 {
+    let (mut max1, mut max2, mut idx) = (0, 0, 0);
+    for (i, &n) in nums.iter().enumerate() {
+        if n > max1 {
+            idx = i;
+            max2 = max1;
+            max1 = n;
+        } else if n > max2 {
+            max2 = n;
+        }
+    }
+    return if max1 >= max2 * 2 { idx as i32 } else { -1 };
+}
+
+/// p748
+pub fn shortest_completing_word(license_plate: String, words: Vec<String>) -> String {
+    let cnt = license_plate
+        .to_lowercase()
+        .chars()
+        .fold(vec![0; 26], |mut acc, c| {
+            if c.is_ascii_alphabetic() {
+                acc[c as u8 as usize - 'a' as usize] += 1;
+            }
+
+            acc
+        });
+
+    let mut ans = "".to_string();
+
+    for word in words {
+        let cnt1 = word.to_lowercase().chars().fold(vec![0; 26], |mut acc, c| {
+            if c.is_ascii_alphabetic() {
+                acc[c as u8 as usize - 'a' as usize] += 1;
+            }
+
+            acc
+        });
+
+        let b = (0..26).all(|i| cnt[i] <= cnt1[i]);
+
+        if b && (ans.is_empty() || ans.len() > word.len()) {
+            ans = word;
+        }
+    }
+
+    ans
+}
+
+/// p749
+
+const DIRS: &'static [isize] = &[0, -1, 0, 1, 0];
+const INFECTED: i32 = 1;
+const UNINFECTED: i32 = 0;
+const BLOCKED: i32 = -1;
+
+pub fn contain_virus(mut grid: Vec<Vec<i32>>) -> i32 {
+    let _len_rs: usize = grid.len();
+    let len_cs: usize = grid[0].len();
+    let mut cnt: i32 = 0;
+    let mut phase: i32 = 1;
+    let mut heap: BinaryHeap<Region> = {
+        let mut heap: BinaryHeap<Region> = BinaryHeap::new();
+        add(phase, &mut grid, &mut heap);
+        heap
+    };
+    while let Some(Region {
+        infected,
+        uninfected_neighbors: _,
+        walls_needed,
+    }) = heap.pop()
+    {
+        cnt += walls_needed;
+        for hash in infected {
+            grid[hash / len_cs][hash % len_cs] = BLOCKED;
+        }
+        phase += 1;
+        while let Some(Region {
+            infected: _,
+            uninfected_neighbors,
+            walls_needed: _,
+        }) = heap.pop()
+        {
+            for hash in uninfected_neighbors {
+                grid[hash / len_cs][hash % len_cs] = phase;
+            }
+        }
+        add(phase, &mut grid, &mut heap);
+    }
+    cnt
+}
+fn add(phase: i32, grid: &mut Vec<Vec<i32>>, heap: &mut BinaryHeap<Region>) {
+    let len_rs: usize = grid.len();
+    let len_cs: usize = grid[0].len();
+    for r in 0..len_rs {
+        for c in 0..len_cs {
+            if grid[r][c] == phase {
+                let mut region = Region::new();
+                dfs((r as isize, c as isize), phase, grid, &mut region);
+                if !region.uninfected_neighbors.is_empty() {
+                    heap.push(region);
+                }
+            }
+        }
+    }
+}
+fn dfs(coord: (isize, isize), phase: i32, grid: &mut Vec<Vec<i32>>, region: &mut Region) {
+    let len_rs: isize = grid.len() as isize;
+    let len_cs: isize = grid[0].len() as isize;
+    let (r, c) = coord;
+    if r < 0
+        || c < 0
+        || r >= len_rs
+        || c >= len_cs
+        || grid[r as usize][c as usize] == -1
+        || grid[r as usize][c as usize] > phase
+    {
+        return;
+    }
+    if grid[r as usize][c as usize] == UNINFECTED {
+        region.uninfected_neighbors.insert(hash(r, c, len_cs));
+        region.walls_needed += 1;
+        return;
+    }
+    grid[r as usize][c as usize] += 1;
+    region.infected.insert(hash(r, c, len_cs));
+    for d in 0..4 {
+        let r_nxt: isize = r + DIRS[d];
+        let c_nxt: isize = c + DIRS[d + 1];
+        dfs((r_nxt, c_nxt), phase, grid, region);
+    }
+}
+fn hash(r: isize, c: isize, len_cs: isize) -> usize {
+    (r * len_cs + c) as usize
+}
+
+struct Region {
+    infected: HashSet<usize>,
+    uninfected_neighbors: HashSet<usize>,
+    walls_needed: i32,
+}
+
+impl Region {
+    pub fn new() -> Self {
+        Region {
+            infected: HashSet::new(),
+            uninfected_neighbors: HashSet::new(),
+            walls_needed: 0,
+        }
+    }
+}
+
+impl Ord for Region {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.uninfected_neighbors
+            .len()
+            .cmp(&other.uninfected_neighbors.len())
+    }
+}
+
+impl PartialOrd for Region {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Region {
+    fn eq(&self, other: &Self) -> bool {
+        self.uninfected_neighbors.len() == other.uninfected_neighbors.len()
+    }
+}
+
+impl Eq for Region {}
