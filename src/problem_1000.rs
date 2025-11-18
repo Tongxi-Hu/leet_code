5caf4f9096e5bf7a643b0f99fcb3077cc920e301
tongxi_hu <tongxi_hu@jiliguala.com>
2025-11-18 13:50:00 +0800

    910
    Signed-off-by: tongxi_hu <tongxi_hu@jiliguala.com>

--- src/problem_1000.rs
+++ src/problem_1000.rs
@@ -230,3 +230,16 @@ pub fn snakes_and_ladders(board: Vec<Vec<i32>>) -> i32 {
     }
     -1
 }
+
+///910
+pub fn smallest_range_ii(mut nums: Vec<i32>, k: i32) -> i32 {
+    nums.sort_unstable();
+    let n = nums.len();
+    let mut ans = nums[n - 1] - nums[0];
+    for i in 1..n {
+        let mx = (nums[i - 1] + k).max(nums[n - 1] - k);
+        let mn = (nums[0] + k).min(nums[i] - k);
+        ans = ans.min(mx - mn);
+    }
+    ans
+}
