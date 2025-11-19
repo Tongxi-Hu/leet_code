9bcb59f837f597c30e6ad33b31c8650750d18d60
tongxi_hu <tongxi_hu@jiliguala.com>
2025-11-19 15:03:11 +0800

    lint
    Signed-off-by: tongxi_hu <tongxi_hu@jiliguala.com>

--- src/problem_1000.rs
+++ src/problem_1000.rs
@@ -236,7 +236,7 @@ pub fn smallest_range_ii(mut nums: Vec<i32>, k: i32) -> i32 {
     nums.sort();
     let n = nums.len();
     let mut ans = nums[n - 1] - nums[0];
-    for i in 1..n {
+    for i in (1..n).into_iter() {
         let mx = (nums[i - 1] + k).max(nums[n - 1] - k);
         let mn = (nums[0] + k).min(nums[i] - k);
         ans = ans.min(mx - mn);
