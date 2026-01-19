ae830b14a3bec1e32366129d9dfcfff187bc06c5
tongxi_hu <tongxi_hu@jiliguala.com>
2026-01-19 13:08:46 +0800

    1103 1104
    Signed-off-by: tongxi_hu <tongxi_hu@jiliguala.com>

---
+++ src/problem_1200.rs
@@ -0,0 +1,28 @@
+/// 1103
+pub fn distribute_candies(mut candies: i32, n: i32) -> Vec<i32> {
+    let n = n as usize;
+    let mut ans = vec![0; n];
+    let mut i = 1;
+    while candies > 0 {
+        ans[(i - 1) as usize % n] += i.min(candies);
+        candies -= i;
+        i += 1;
+    }
+    ans
+}
+
+/// 1104
+pub fn path_in_zig_zag_tree(label: i32) -> Vec<i32> {
+    let mut x = 2_i32.pow((label as f64).log2() as u32);
+    let mut label = label;
+    let mut ret = vec![label];
+
+    while label > 1 {
+        label = x - 1 - ((label / 2) % (x / 2));
+        x /= 2;
+        ret.push(label);
+    }
+
+    ret.reverse();
+    ret
+}
