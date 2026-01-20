9ad1d8ddd2e672d82d562ccb43573354dad0aade
tongxi_hu <tongxi_hu@jiliguala.com>
2026-01-20 12:20:09 +0800

    1106 1108 1109
    Signed-off-by: tongxi_hu <tongxi_hu@jiliguala.com>

--- src/problem_1200.rs
+++ src/problem_1200.rs
@@ -1,3 +1,11 @@
+use std::{
+    cell::RefCell,
+    collections::{HashMap, VecDeque},
+    rc::Rc,
+};
+
+use crate::common::TreeNode;
+
 /// 1103
 pub fn distribute_candies(mut candies: i32, n: i32) -> Vec<i32> {
     let n = n as usize;
@@ -43,3 +51,124 @@ pub fn min_height_shelves(books: Vec<Vec<i32>>, shelf_width: i32) -> i32 {
     }
     dp[n]
 }
+
+/// 1106
+pub fn parse_bool_expr(expression: String) -> bool {
+    let mut stack = VecDeque::new();
+    expression.chars().for_each(|c| match c {
+        '(' | 't' | 'f' | '!' | '&' | '|' => {
+            stack.push_back(c);
+        }
+        ')' => {
+            let mut list = vec![];
+            while *stack.back().unwrap() != '(' {
+                list.push(stack.pop_back().unwrap())
+            }
+            stack.pop_back();
+            match stack.pop_back().unwrap() {
+                '&' => {
+                    if list.contains(&'f') {
+                        stack.push_back('f')
+                    } else {
+                        stack.push_back('t')
+                    }
+                }
+                '!' => {
+                    if list.contains(&'f') {
+                        stack.push_back('t')
+                    } else {
+                        stack.push_back('f')
+                    }
+                }
+                '|' => {
+                    if list.contains(&'t') {
+                        stack.push_back('t')
+                    } else {
+                        stack.push_back('f')
+                    }
+                }
+                _ => (),
+            }
+        }
+        ',' => (),
+        _ => (),
+    });
+    stack.pop_back().unwrap() == 't'
+}
+
+/// 1108
+pub fn defang_i_paddr(address: String) -> String {
+    address.replace(".", "[.]")
+}
+
+/// 1109
+pub fn corp_flight_bookings(bookings: Vec<Vec<i32>>, n: i32) -> Vec<i32> {
+    let (mut get_on, mut get_off) = (HashMap::new(), HashMap::new());
+    bookings.iter().for_each(|b| {
+        let on = get_on.entry(b[0]).or_insert(0);
+        *on = *on + b[2];
+        let off = get_off.entry(b[1] + 1).or_insert(0);
+        *off = *off + b[2];
+    });
+    let (mut total, mut time_line) = (0, vec![0; n as usize]);
+    time_line.iter_mut().enumerate().for_each(|(day, cnt)| {
+        let (on, off) = (
+            get_on.get(&((day + 1) as i32)).unwrap_or(&0),
+            get_off.get(&((day + 1) as i32)).unwrap_or(&0),
+        );
+        total = total + on - off;
+        *cnt = total;
+    });
+    time_line
+}
+
+// /// 1110
+// pub fn del_nodes(
+//     root: Option<Rc<RefCell<TreeNode>>>,
+//     mut to_delete: Vec<i32>,
+// ) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
+//     let mut trees = vec![root];
+//     fn delete_target(
+//         target: i32,
+//         tree: Option<Rc<RefCell<TreeNode>>>,
+//     ) -> (bool, Vec<Option<Rc<RefCell<TreeNode>>>>) {
+//         let mut new_tree = vec![];
+//         if let Some(node) = tree.as_ref() {
+//             if node.borrow().val == target {
+//                 if node.borrow().left.is_some() {
+//                     new_tree.push(node.borrow().left.clone());
+//                 }
+//                 if node.borrow().right.is_some() {
+//                     new_tree.push(node.borrow().right.clone());
+//                 }
+//                 node.borrow_mut().left = None;
+//                 node.borrow_mut().right = None;
+//                 return (true, new_tree);
+//             } else {
+//                 let (left_removed, mut left_tree) =
+//                     delete_target(target, node.borrow().left.clone());
+//                 let (right_memoved, mut right_tree) =
+//                     delete_target(target, node.borrow().right.clone());
+//                 if left_removed {
+//                     node.borrow_mut().left = None;
+//                 }
+//                 if right_memoved {
+//                     node.borrow_mut().right = None;
+//                 }
+//                 new_tree.push(tree);
+//                 new_tree.append(&mut left_tree);
+//                 new_tree.append(&mut right_tree);
+//             }
+//         }
+//         (false, new_tree)
+//     }
+//     while to_delete.len() != 0 {
+//         let target = to_delete.pop().unwrap();
+//         let size = trees.len();
+//         for _ in 0..size {
+//             let (_, mut new_tree) = delete_target(target, trees.remove(0));
+//             trees.append(&mut new_tree);
+//         }
+//     }
+//     trees
+// }
