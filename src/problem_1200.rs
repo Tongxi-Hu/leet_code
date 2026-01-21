ae743ee97dfc95591351d1fa356e3ce1c5c1979e
tongxi_hu <tongxi_hu@jiliguala.com>
2026-01-21 15:17:44 +0800

    1128 1129 1130
    Signed-off-by: tongxi_hu <tongxi_hu@jiliguala.com>

--- src/problem_1200.rs
+++ src/problem_1200.rs
@@ -365,3 +365,95 @@ pub fn smallest_sufficient_team(req_skills: Vec<String>, people: Vec<Vec<String>
     }
     dp[(1 << n) - 1].as_ref().unwrap().clone()
 }
+
+/// 1128
+pub fn num_equiv_domino_pairs(dominoes: Vec<Vec<i32>>) -> i32 {
+    let cnt = dominoes.iter().fold(HashMap::new(), |mut acc, cur| {
+        let tag = if cur[0] <= cur[1] {
+            (cur[0], cur[1])
+        } else {
+            (cur[1], cur[0])
+        };
+        let cnt = acc.entry(tag).or_insert(0);
+        *cnt = *cnt + 1;
+        acc
+    });
+    cnt.values().fold(0, |acc, cur| acc + cur * (cur - 1) / 2)
+}
+
+/// 1129
+pub fn shortest_alternating_paths(
+    n: i32,
+    red_edges: Vec<Vec<i32>>,
+    blue_edges: Vec<Vec<i32>>,
+) -> Vec<i32> {
+    use std::collections::{HashSet, VecDeque};
+    fn build_graph(
+        n: usize,
+        red_edges: &Vec<Vec<i32>>,
+        blue_edges: &Vec<Vec<i32>>,
+    ) -> Vec<Vec<i32>> {
+        let mut graph = vec![vec![-(n as i32); n as usize]; n as usize];
+        for re in red_edges {
+            graph[re[0] as usize][re[1] as usize] = 1;
+        }
+        for be in blue_edges {
+            graph[be[0] as usize][be[1] as usize] = if graph[be[0] as usize][be[1] as usize] == 1 {
+                0
+            } else {
+                -1
+            };
+        }
+        graph
+    }
+    let (graph, mut queue, mut visited, mut answer, mut len) = (
+        build_graph(n as usize, &red_edges, &blue_edges),
+        VecDeque::new(),
+        HashSet::new(),
+        vec![i32::MAX; n as usize],
+        0,
+    );
+    queue.push_back((0, 1));
+    queue.push_back((0, -1));
+    answer[0] = 0;
+    while !queue.is_empty() {
+        len += 1;
+        let size = queue.len();
+        for _ in 0..size {
+            let curr = queue.pop_front().unwrap();
+            let (node, opposite_color) = (curr.0, -curr.1);
+            for i in 0..n as usize {
+                if graph[node][i] != opposite_color && graph[node][i] != 0 {
+                    continue;
+                }
+                if !visited.insert((i, opposite_color)) {
+                    continue;
+                }
+                queue.push_back((i as usize, opposite_color));
+                answer[i] = answer[i].min(len);
+            }
+        }
+    }
+    for i in 1..n as usize {
+        answer[i] = if answer[i] == i32::MAX { -1 } else { answer[i] };
+    }
+    answer
+}
+
+/// 1130
+pub fn mct_from_leaf_values(arr: Vec<i32>) -> i32 {
+    use std::collections::VecDeque;
+    let (mut queue, mut min_sum) = (VecDeque::new(), 0);
+    queue.push_back(i32::MAX);
+    for a in arr {
+        while queue.back().unwrap() <= &a {
+            let top = queue.pop_back().unwrap();
+            min_sum += top * a.min(*queue.back().unwrap());
+        }
+        queue.push_back(a)
+    }
+    while queue.len() > 2 {
+        min_sum += queue.pop_back().unwrap() * queue.back().unwrap();
+    }
+    min_sum
+}
