2d815176c9959ef6a9775be457fe45cca8f66ea1
tongxi_hu <tongxi_hu@jiliguala.com>
2026-01-23 11:27:45 +0800

    1155
    Signed-off-by: tongxi_hu <tongxi_hu@jiliguala.com>

--- src/problem_1200.rs
+++ src/problem_1200.rs
@@ -684,3 +684,23 @@ pub fn day_of_year(date: String) -> i32 {
     };
     total + day
 }
+
+/// 1155
+pub fn num_rolls_to_target(n: i32, k: i32, target: i32) -> i32 {
+    let (n, k, target) = (n as usize, k as usize, target as usize);
+    let mut dp = vec![vec![0; n + 1]; target + 1];
+    dp[0][0] = 1;
+    for t in 1..=target.min(n * k) {
+        for r in 1..=n {
+            for i in 1..=t.min(k) {
+                dp[t][r] = (dp[t][r] + dp[t - i][r - 1]) % 1_000_000_007
+            }
+        }
+    }
+    dp[target][n]
+}
+
+#[test]
+fn test_1200() {
+    num_rolls_to_target(3, 6, 6);
+}
