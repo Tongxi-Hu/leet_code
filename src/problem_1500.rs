666a398b50caa6118979457a278039f05086ecc9
tongxi hu <ryan.proning@gmail.com>
2026-02-14 11:52:25 +0800

    19 20

--- src/problem_1500.rs
+++ src/problem_1500.rs
@@ -384,3 +384,79 @@ pub fn display_table(orders: Vec<Vec<String>>) -> Vec<Vec<String>> {
     });
     result
 }
+
+/// 19
+pub fn min_number_of_frogs(croak_of_frogs: String) -> i32 {
+    let (mut container, mut valid) = (vec![], true);
+    croak_of_frogs.chars().for_each(|c| match c {
+        'c' => {
+            if let Some((i, _)) = container.iter().enumerate().find(|(_, c)| **c == 'k') {
+                container[i] = 'c';
+            } else {
+                container.push('c');
+            }
+        }
+        'r' => {
+            if let Some((i, _)) = container.iter().enumerate().find(|(_, c)| **c == 'c') {
+                container[i] = 'r';
+            } else {
+                valid = false;
+            }
+        }
+        'o' => {
+            if let Some((i, _)) = container.iter().enumerate().find(|(_, c)| **c == 'r') {
+                container[i] = 'o';
+            } else {
+                valid = false;
+            }
+        }
+        'a' => {
+            if let Some((i, _)) = container.iter().enumerate().find(|(_, c)| **c == 'o') {
+                container[i] = 'a';
+            } else {
+                valid = false;
+            }
+        }
+        'k' => {
+            if let Some((i, _)) = container.iter().enumerate().find(|(_, c)| **c == 'a') {
+                container[i] = 'k';
+            } else {
+                valid = false;
+            }
+        }
+        _ => (),
+    });
+    valid = valid && container.iter().all(|c| *c == 'k');
+    return if valid { container.len() as i32 } else { -1 };
+}
+
+/// 20
+pub fn num_of_arrays(n: i32, m: i32, k: i32) -> i32 {
+    let mood = 1000000007;
+    let (n, m, k) = (n as usize, m as usize, k as usize);
+    let mut dp = vec![vec![vec![0; 55]; 105]; 105];
+    for j in 1..m + 1 {
+        dp[1][j][1] = 1;
+    }
+    for i in 1..n + 1 {
+        for j in 1..m + 1 {
+            for t in 1..k + 1 {
+                for _ in 1..j + 1 {
+                    dp[i + 1][j][t] += dp[i][j][t];
+                    dp[i + 1][j][t] %= mood;
+                }
+                for jj in j + 1..m + 1 {
+                    dp[i + 1][jj][t + 1] += dp[i][j][t];
+                    dp[i + 1][jj][t + 1] %= mood;
+                }
+            }
+        }
+    }
+
+    let mut result = 0;
+    for i in 1..m + 1 {
+        result += dp[n][i][k];
+        result %= mood;
+    }
+    result
+}
