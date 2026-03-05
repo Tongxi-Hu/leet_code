303b8978e6e5fc02048e7df4978205f4edf6632e
tongxi hu <ryan.proning@gmail.com>
2026-03-05 15:36:08 +0800

    47

--- src/problem_1600.rs
+++ src/problem_1600.rs
@@ -832,6 +832,26 @@ pub fn max_non_overlapping(nums: Vec<i32>, target: i32) -> i32 {
     cnt
 }
 
+/// 47
+pub fn min_cost(n: i32, cuts: Vec<i32>) -> i32 {
+    let mut cuts = cuts;
+    cuts.push(0);
+    cuts.push(n);
+    cuts.sort();
+    let m = cuts.len();
+    let mut dp = vec![vec![0; m]; m];
+    for i in (0..m).rev() {
+        for j in i + 2..m {
+            dp[i][j] = i32::MAX;
+            for k in i + 1..j {
+                dp[i][j] = dp[i][j].min(dp[i][k] + dp[k][j]);
+            }
+            dp[i][j] += cuts[j] - cuts[i];
+        }
+    }
+    dp[0][m - 1]
+}
+
 /// 50
 pub fn three_consecutive_odds(arr: Vec<i32>) -> bool {
     arr.windows(3)
