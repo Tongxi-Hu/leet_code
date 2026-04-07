6b3dd8f74174f629cc4127c644d9099364df18c6
tongxi hu <ryan.proning@gmail.com>
2026-04-07 09:26:51 +0800

    01 02 03 04 05

---
+++ src/problem_1900.rs
@@ -0,0 +1,176 @@
+use std::{
+    cmp::{Ordering, Reverse},
+    collections::{BinaryHeap, HashSet},
+};
+
+/// 01
+pub fn get_number_of_backlog_orders(orders: Vec<Vec<i32>>) -> i32 {
+    #[derive(Eq, Debug, Clone, Default)]
+    struct Order {
+        price: i32,
+        amount: i32,
+        order_type: i32,
+    }
+
+    impl Order {
+        fn new(price: i32, amount: i32, order_type: i32) -> Self {
+            Order {
+                price,
+                amount,
+                order_type,
+            }
+        }
+    }
+
+    impl PartialEq for Order {
+        fn eq(&self, other: &Self) -> bool {
+            self.price == other.price
+        }
+    }
+
+    impl PartialOrd for Order {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+
+    impl Ord for Order {
+        fn cmp(&self, other: &Self) -> Ordering {
+            self.price.cmp(&other.price)
+        }
+    }
+
+    const MOD: i32 = 1000000007;
+    let (buy, sell) = orders
+        .iter()
+        .fold((BinaryHeap::new(), BinaryHeap::new()), |mut acc, cur| {
+            if cur[2] == 0 {
+                acc.0.push(Order::new(cur[0], cur[1], cur[2]));
+            } else {
+                acc.1.push(Reverse(Order::new(cur[0], cur[1], cur[2])));
+            }
+            while !acc.0.is_empty()
+                && !acc.1.is_empty()
+                && acc.0.peek().unwrap().price >= acc.1.peek().unwrap().0.price
+            {
+                let amount = acc
+                    .0
+                    .peek()
+                    .unwrap()
+                    .amount
+                    .min(acc.1.peek().unwrap().0.amount);
+                acc.0.peek_mut().unwrap().amount -= amount;
+                acc.1.peek_mut().unwrap().0.amount -= amount;
+                if acc.0.peek().unwrap().amount == 0 {
+                    acc.0.pop();
+                }
+                if acc.1.peek().unwrap().0.amount == 0 {
+                    acc.1.pop();
+                }
+            }
+            (acc.0, acc.1)
+        });
+    let cnt = buy.iter().fold(0, |cnt, o| (cnt + o.amount) % MOD);
+    sell.iter().fold(cnt, |cnt, o| (cnt + o.0.amount) % MOD)
+}
+
+/// 02
+pub fn max_value(n: i32, index: i32, max_sum: i32) -> i32 {
+    let mut lo = 0;
+    let mut hi = max_sum;
+    let rest = max_sum as i64 - n as i64;
+
+    while lo <= hi {
+        let mid = lo + (hi - lo) / 2;
+
+        let left_cnt = (mid + 1.max(mid - index)) as i64 * (mid.min(index + 1)) as i64 / 2; // [0..=index]的总高度
+        let right_cnt =
+            (mid + 1.max(mid - (n - 1 - index))) as i64 * (mid.min(n - index)) as i64 / 2; // [index..n]的总高度
+
+        if left_cnt + right_cnt - mid as i64 > rest {
+            hi = mid - 1;
+        } else {
+            lo = mid + 1;
+        }
+    }
+
+    lo
+}
+
+/// 03
+pub fn count_pairs(nums: Vec<i32>, low: i32, high: i32) -> i32 {
+    fn f(nums: &Vec<i32>, x: i32) -> i32 {
+        let mut root = Trie::new();
+        let mut ans = 0;
+
+        for i in 1..nums.len() {
+            root.insert(nums[i - 1]);
+            ans += root.search(nums[i], x);
+        }
+
+        ans
+    }
+
+    f(&nums, high) - f(&nums, low - 1)
+}
+
+#[derive(Default)]
+struct Trie {
+    children: [Option<Box<Trie>>; 2],
+    sum: i32,
+}
+
+impl Trie {
+    fn new() -> Self {
+        Default::default()
+    }
+
+    fn insert(&mut self, num: i32) {
+        let mut p = self;
+
+        for k in (0..=14).rev() {
+            let b = ((num >> k) & 1) as usize;
+            p = p.children[b].get_or_insert_with(|| Box::new(Trie::new()));
+            p.sum += 1
+        }
+    }
+
+    fn search(&self, num: i32, x: i32) -> i32 {
+        let mut p = self;
+        let mut sum = 0;
+
+        for k in (0..=14).rev() {
+            let r = ((num >> k) & 1) as usize;
+
+            if ((x >> k) & 1) != 0 {
+                if let Some(ref child) = p.children[r] {
+                    sum += child.sum;
+                }
+                if let Some(ref child) = p.children[r ^ 1] {
+                    p = child;
+                } else {
+                    return sum;
+                }
+            } else {
+                if let Some(ref child) = p.children[r] {
+                    p = child;
+                } else {
+                    return sum;
+                }
+            }
+        }
+
+        sum += p.sum;
+
+        sum
+    }
+}
+
+/// 05
+pub fn num_different_integers(word: String) -> i32 {
+    word.split(|c: char| !c.is_ascii_digit())
+        .filter(|s| !s.is_empty())
+        .map(|s| s.trim_start_matches('0'))
+        .collect::<HashSet<_>>()
+        .len() as i32
+}
